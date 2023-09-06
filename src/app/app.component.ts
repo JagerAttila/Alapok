34f1b938f95c5d01d1cb78efb14a2e1a19b7a6bc
Nagy János <janos@zold.lan>
2023-09-06 09:27:14 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Alapok';
+
+  oszlopok =[
+    {key:"name",texthu:"Név"},
+    {key:"age", texthu:"Kor"},
+    {key:"hair", texthu:"Hallyszínw"},
+  ]
+
+  obj:any =[
+    {name:"Béla", age:31, hair:"barna"},
+    {name:"Attila", age:48, hair:"szőke"},
+    {name:"Dezső", age:55, hair:"fekete"},
+    {name:"Józsi", age:55, hair:"fekete"},
+    {name:"András", age:56, hair:"barna"},
+  ];
+
+
+  torol(tancsi:any, i:number){
+    console.log("i:",i)
+    console.log("Kitörlendő: ", tancsi)
+    //this.obj.splice(i,1)
+    this.obj=this.obj.filter(
+      (e:any) => e!=tancsi)
+  }
 }
